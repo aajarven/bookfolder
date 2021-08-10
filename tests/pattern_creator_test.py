16fa86718393b0d6d26dd42745ef997f43ac2e27
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-08-10 08:32:45 +0000

    Make test output more readable
    Renamed tests so that the module name is not unnecessarily repeated in
    test case name, making the names more compact. Also manually added
    descriptive test IDs for parametrized tests.

--- tests/pattern_creator_test.py
+++ tests/pattern_creator_test.py
@@ -8,46 +8,45 @@ from bookfolder.pattern_creator import PatternCreator
 # test functions should be self-documenting
 
 
-def test_pattern_creator_input_path():
+def test_input_path():
     creator = PatternCreator("some/path.png")
     assert creator.input_path == "some/path.png"
 
 
-def test_pattern_creator_default_measurement_interval():
+def test_default_measurement_interval():
     creator = PatternCreator("some/path.png")
     assert creator.measurement_interval == 0.25
 
 
-def test_pattern_creator_measurement_interval_initialization():
+def test_measurement_interval_initialization():
     creator = PatternCreator("some/path.png", 1.0)
     assert creator.measurement_interval == 1.0
 
 
-def test_pattern_creator_extracts_correct_number_of_sheets():
+def test_extracts_correct_number_of_sheets():
     creator = PatternCreator("tests/data/simple_pattern.png")
     assert len(creator.sheets()) == 3
 
 
-def test_pattern_creator_extracted_sheets_have_correct_number_of_folds():
+def test_extracted_sheets_have_correct_number_of_folds():
     creator = PatternCreator("tests/data/simple_pattern.png")
     assert len(creator.sheets()[0].folds) == 3
     assert len(creator.sheets()[1].folds) == 5
     assert len(creator.sheets()[2].folds) == 4
 
 
-def test_pattern_creator_extracted_sheets_have_correct_folds_locations():
+def test_extracted_sheets_have_correct_folds_locations():
     creator = PatternCreator("tests/data/simple_pattern.png")
     assert creator.sheets()[0].folds == [4, 6, 7]
 
 
-def test_pattern_creator_extracted_multicolor_sheets_have_correct_number_of_folds():  # noqa: E501
-    # pylint: disable=line-too-long
+def test_extracted_multicolor_sheets_have_correct_number_of_folds():
     creator = PatternCreator("tests/data/color_pattern.png")
     assert len(creator.sheets()[0].folds) == 5
     assert len(creator.sheets()[1].folds) == 5
     assert len(creator.sheets()[2].folds) == 4
 
 
-def test_pattern_creator_extracted_color_sheets_have_correct_folds_locations():
+def test_extracted_color_sheets_have_correct_folds_locations():
     creator = PatternCreator("tests/data/color_pattern.png")
     assert creator.sheets()[0].folds == [1, 3, 4, 6, 7]
--- tests/sheet_test.py
+++ tests/sheet_test.py
@@ -14,18 +14,18 @@ from bookfolder.sheet import Sheet
 @pytest.mark.parametrize(
     "folds",
     [
-        [],
-        [1, 3, 100],
-        [100, 3, 1],
-        list(range(1000)),
+        pytest.param([], id="empty_folds_list"),
+        pytest.param([1, 3, 100], id="short_folds_list"),
+        pytest.param([100, 3, 1], id="short_unsorted_folds_list"),
+        pytest.param(list(range(1000)), id="long_folds_list"),
     ]
 )
-def test_create_sheet_has_correct_folds(folds):
+def test_has_correct_folds(folds):
     sheet = Sheet(folds, measurement_interval=0.1)
     assert len(sheet.folds) == len(folds)
     assert set(sheet.folds) == set(folds)
 
 
-def test_create_sheet_sorts_folds():
+def test_folds_are_sorted():
     sheet = Sheet([1, 40, 2, 45, 3], measurement_interval=0.1)
     assert sheet.folds == [1, 2, 3, 40, 45]
