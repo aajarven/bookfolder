06d64967654d893f7fd54ebe5da9fe42d90099b1
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2021-08-18 10:41:39 +0000

    Make build pypi-compatible

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="bookfolder",
     version="0.1",
+    author="Anni Järvenpää",
+    description="Convert an image into a bookfolding pattern",
+    url="https://github.com/aajarven/bookfolder",
     packages=find_packages(),
+    python_requires=">=3.6",
     include_package_data=True,
     install_requires=[
         "click",
